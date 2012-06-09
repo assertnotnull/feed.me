6c132a0f80925c358cf7620f89c90a90da6801a4
devsoftweb <devin@devsoftweb.com>
2012-06-09 15:56:48 -0400

    added markers to map

--- socalcore/search.py
+++ socalcore/search.py
@@ -12,11 +12,13 @@ class Search:
 		data = api.find_business('Restaurant ' + arg, 'cZ' + location, 123, page_len=5);
 
 		listings = json.loads(data)['listings']
-		
+			
+		script = "<script type=''text/javascript'>"
 		ret = "<ul>"
 		for business in listings:
 			ret += "<li>" + business['name'] + "</li>"
+			script += "map.addMarker({ lat: " + business['geoCode']['latitude'] + ",  lng: " + business['geoCode']['longitude'] + ", title: '" + business['name'] + "'});"
 			
 		ret += "</ul>"
 		
-		return ret
+		return ret + script;
