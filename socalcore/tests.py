51f40a5affb85e47ad89d1d3d5cd5f69683667a0
assertnotnull <branwulf@pestenoire.(none)>
2012-06-14 09:15:41 -0400

    update tests again

--- socalcore/tests.py
+++ socalcore/tests.py
@@ -15,12 +15,13 @@ class SimpleTest(TestCase):
     location = 'montreal'
 
     def test_yellow_api(self):
+        #TODO a mocker
         yellow = YellowAPI('s73bf2pqaswz5a6secydtsth', test_mode=True, format='JSON')
         result = yellow_search(yellow, self.terme, self.location)
         self.assertEqual(result[0].get('name'), 'Restaurant Frites Alors')
 
     def test_wajam_api(self):
+        #TODO a mocker
         wajamurl = 'https://api.wajam.com/trial/v1/search?q={0}'
         result = wajam_search(wajamurl, 'suchi')
-        print result
         self.assertEqual(result.get('total_found'), 0)
